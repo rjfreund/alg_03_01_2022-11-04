ad56bc1da246a2c60a73dac57006a67374eda5d2
R.J. Freund <robert.joseph.freund@gmail.com>
2022-11-04 18:34:14 -0500

    -

---
+++ index.js
@@ -0,0 +1,16 @@
+function cut_rod(rod_length_inches, prices, gold_inches) {
+
+}
+
+function get_rod_cut_price_combinations(rod_length_inches, prices) {
+    var rod_cut_price_combinations = {};
+    for (var i = 0; i < prices.length; i++) {
+        rod_cut_price_combinations[prices[rod_length_inches - 1]]
+    }
+}
+
+var rod_length_inches = 10;
+var prices = [1, 14, 35, 44, 45, 60, 91, 110, 117, 158];
+var gold_inches = 4;
+
+console.log(cut_rod)
